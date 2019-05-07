a2037aff92c0de1032b6d072b83d4ab128620168
Michael Erazo <merazo1987@gmail.com>
2019-05-07 00:37:57 +0000

    lesson 8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,11 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require_tree .
+//= require jquery3
+
